aaa42d192cec15b5bdfdb1512cf4d90ecb53b69c
stoflom <stoflom@gmail.com>
2022-03-11 14:51:14 +0200

    Change trees to tree

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ import * as express from 'express';
 import { connect } from './database/database';
 import { json } from 'body-parser';
 import { urlencoded } from 'body-parser';
+
 import { theRouter } from './routes/routes';
+
 //import Morgan = require('morgan');
 
 const app = express();
@@ -12,6 +14,8 @@ app.use(json({ strict: true }));
 app.use(urlencoded({ extended: false }));
 //app.use(Morgan('combined')) ;
 
+
+//Register the router
 app.use(theRouter);
 
 connect();
