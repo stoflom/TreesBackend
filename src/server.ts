41cbd0e7e0e25c275bea25d5652e16bd714f5a73
stoflom <stoflom@gmail.com>
2024-11-20 20:26:44 +0200

    Added cors to express

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import * as express from 'express';
+
 import { connect } from './database/database';
 import { json } from 'body-parser';
 import { urlencoded } from 'body-parser';
@@ -7,9 +7,13 @@ import { theRouter } from './routes/routes';
 
 //import Morgan = require('morgan');
 
-const app = express();
+import express = require('express');
+import cors = require('cors');
+
 const port = 5002;
 
+const app =express();
+app.use(cors());
 app.use(json({ strict: true }));
 app.use(urlencoded({ extended: false }));
 //app.use(Morgan('combined')) ;
